e3bf03da6061e85960803b4f896fa03851d55c5f
“rweddell” <rweddell4@gmail.com>
2022-06-16 16:54:32 -0500

    Added tests for expense creation

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,11 +1,49 @@
-# from django.test import TestCase
-from unittest import TestCase
+from django.test import TestCase
+from django.urls import reverse
+
+# from unittest import TestCase
+from restapi import models
 
 
-def two_integers_sum(a, b):
-    return a + b
+class TestModel(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=249.99,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
+
+        self.assertEqual(249.99, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("anc headphones", inserted_expense.description)
+        self.assertEqual("music", inserted_expense.category)
 
 
-class TestSum(TestCase):
-    def test_sum(self):
-        self.assertEqual(two_integers_sum(1, 2), 3)
+class TestViews(TestCase):
+    def test_expense_create(self):
+        payload = {
+            "amount": "50",
+            "merchant": "AT&T",
+            "description": "cell phone subscription",
+            "category": "utilties",
+        }
+        res = self.client.post(
+            reverse("restapi:expense_list_create"), payload, format="json"
+        )
+        self.assertEqual(201, res.status_code)
+        json_res = res.json()
+        self.assertEqual(payload["amount"], json_res["amount"])
+        self.assertEqual(payload["merchant"], json_res["merchant"])
+        self.assertEqual(payload["description"], json_res["description"])
+        self.assertEqual(payload["category"], json_res["category"])
+        self.assertIsInstance(json_res["id"], int)
+
+    def test_expense_list(self):
+        res = self.client.get(reverse("restapi:expense_list_create"), format="json")
+        self.assertEqual(200, res.status_code)
+        json_res = res.json()
+        self.assertIsInstance(json_res, list)
+        expenses = models.Expense.objects.all()
+        self.assertEqual(len(expenses), len(json_res))
